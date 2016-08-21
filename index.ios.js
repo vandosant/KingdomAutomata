ba0bf963f97bfbfc60535d8e506f7b35f0921763
vandosant <sjskender@gmail.com>
2016-08-21 14:07:37 -0600

    Clone android to ios

--- index.ios.js
+++ index.ios.js
@@ -9,20 +9,38 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  TouchableHighlight
 } from 'react-native';
 
 class KingdomAutomata extends Component {
+  constructor() {
+    super();
+    this.state = {
+      plants: ['a','b','c'],
+      newSearch: ''
+    };
+  }
+  handleChange(value) {
+    this.setState({newSearch: value});
+  }
+  handlePress() {
+    const plants = [...this.state.plants, this.state.newSearch]
+    this.setState({plants, newSearch: ''});
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to KingdomAutomata!
         </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+	<TouchableHighlight onPress={this.handlePress.bind(this)}>
+          <Text>Search</Text>
+	</TouchableHighlight>
+	<TextInput value={this.state.newSearch} onChangeText={this.handleChange.bind(this)}/>
+        {this.state.plants.map(plant => <Text>{plant}</Text>)}
+	<Text>{this.state.search}</Text>
       </View>
     );
   }
